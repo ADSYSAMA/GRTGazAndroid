c4c05d1988a2b1b896528ffe16dd911c21602815
Guillaume <fuditphuguillaume@gmail.com>
2018-01-14 21:22:40 +0100

    bouton pour la carte

--- app/src/main/java/com/example/adsy/grtgaz/MainActivity.java
+++ app/src/main/java/com/example/adsy/grtgaz/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.adsy.grtgaz;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -15,11 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bouton_carte = (Button)findViewById(R.id.boutonMap);
 
+        bouton_carte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            }
+        });
 
     }
+
 }
 /*Je fais un commit inutile
 * */
